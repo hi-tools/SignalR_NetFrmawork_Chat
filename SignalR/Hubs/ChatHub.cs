44517c3321fa88b848ea6f45f78f91aa3e72f921
admin <>
2020-01-12 10:29:46 +0330

    Init

---
+++ SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void Send(string name, string message)
+        {
+            Clients.All.addNewMessageToPage(name, message);
+        }
+        public void Test(string message)
+        {
+            Clients.All.Test(message);
+        }
+
+        public void SendToUser(string user, string message)
+        {
+            //Context.User.Identity.Name;
+            Clients.All.Test(message);
+        }
+    }
+}
